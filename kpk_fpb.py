369be3a4a9da0a488b968f224a76888505ea8038
salmaasmanadia <salma.asma.nadia@mail.ugm.ac.id>
2023-02-18 17:15:41 +0700

    create kpk fpb and roman converter program

---
+++ kpk_fpb.py
@@ -0,0 +1,37 @@
+# mencari kpk dan fpb dengan library python : numpy
+# import numpy as np
+
+# a = input('Masukkan Angka pertama: ')
+# b = input('Masukkan Angka kedua: ')
+
+# num1 = int(a)
+# num2 = int(b)
+
+# kpk = np.lcm(num1, num2)
+# fpb = np.gcd(num1, num2)
+
+# print('KPK: ' + str(kpk))
+# print('FPB: ' + str(fpb))
+
+# MANUAL
+# langkah 1 : input dua bilangan a dan b
+a = int(input('Masukkan Bilangan Pertama: '))
+b = int(input('Masukkan Bilangan Kedua: '))
+
+# langkah 2 : buat 2 list kosong
+list1=[]
+list2=[]
+
+# lagkah 3 : Masukkan nilai dari 1 sampai dengan a atau b pada list1
+for i in range(1, a+1):
+    list1.append(i)
+# print(list1)
+# langkah 4 : Masukkan nilai  pada list1 yang memenuhi syarat bahwa dibagi habis bilangan a dan b kedalam list2
+for x in list1:
+    if a%x==0 and b%x==0:
+        list2.append(x)
+# print(list2)
+# langkah 5 : FPB adalah elemen terbesar pada list2
+print(f'\nFPB dari {a} dan {b} = {list2[len(list2)-1]}')
+# langkah 6 : KPK adalah hasil kali dua elemen a dan b di bagi dengan FPB dari a dan b
+print('KPK dari {} dan {} = {:.0f}'.format(a,b, a*b/list2[len(list2)-1]))
---
+++ number_to_romawi.py
@@ -0,0 +1,63 @@
+# number to Roman values
+import math
+
+A = int(input('Insert Number (max val = 100 million): '))
+
+def integerToRoman(A):
+    # create a roman numeral dictionary
+	romansDict = \
+		{
+			1: "I",
+			5: "V",
+			10: "X",
+			50: "L",
+			100: "C",
+			500: "D",
+			1000: "M",
+			5000: "V`",
+			10000: "X`",
+            50000: "L`",
+            100000: "C`",
+            500000: "D`",
+            1000000: "M`",
+            # The roman numerals below are just my example
+            # if the number entered by the user reaches 100 million
+            5000000: "V``",
+            10000000: "X``",
+            50000000: "D``",
+            100000000: "C``"
+		}
+    # 
+	div = 1
+	while A >= div:
+		div *= 10
+
+	div /= 10
+
+	res = ""
+
+	while A:
+
+		# main significant digit extracted
+		# into lastNum
+		lastNum = int(A / div)
+
+		if lastNum <= 3:
+			res += (romansDict[div] * lastNum)
+		elif lastNum == 4:
+			res += (romansDict[div] +
+						romansDict[div * 5])
+		elif 5 <= lastNum <= 8:
+			res += (romansDict[div * 5] +
+			(romansDict[div] * (lastNum - 5)))
+		elif lastNum == 9:
+			res += (romansDict[div] +
+						romansDict[div * 10])
+
+		A = math.floor(A % div)
+		div /= 10
+		
+	return res
+
+# Driver code
+print("Roman value for the integer is:" + str(integerToRoman(A)))
